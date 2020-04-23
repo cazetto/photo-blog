9c4f65303d08936d480532cf8ec46af76ea9dffb
André Pesci Cazetto <cazetto.andre@gmail.com>
2020-04-23 18:13:12 -0300

    Added react-router-dom and a basic routing with the app requirement

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { hot } from 'react-hot-loader/root';
+import UserPage from './features/User/UserPage';
 
 const App = () => (
-  <>
-    <input type="text" />
-    <div>Hello World!</div>
-  </>
+  <Router>
+    <Switch>
+      <Route path="/:userId">
+        <UserPage />
+      </Route>
+    </Switch>
+  </Router>
 );
 
 export default process.env.NODE_ENV === 'development' ? hot(App) : App;
