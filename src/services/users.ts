c9b0d8c9f977c136a651eb13872ef04b1d3abed4
André Pesci Cazetto <cazetto.andre@gmail.com>
2020-04-27 00:24:46 -0300

    Add customer posts

--- src/services/users.ts
+++ src/services/users.ts
@@ -9,3 +9,8 @@ const USER_PHOTOS_URI = '/photos';
 export function getUserPhotos(userId: string) {
   return axiosInstance.get(`${USER_PHOTOS_URI}?albumId=${userId}`);
 }
+
+const USER_POSTS_URI = '/posts';
+export function getUserPosts(userId: string) {
+  return axiosInstance.get(`${USER_POSTS_URI}?userId=${userId}`);
+}
