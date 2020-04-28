7af1cce9e78f4a4af8fa1a83c2f5d2470d849a89
André Pesci Cazetto <cazetto.andre@gmail.com>
2020-04-28 16:59:07 -0300

    Fix ImageSlider and make it responsible

--- src/features/Customer/components/CustomerInfo.tsx
+++ src/features/Customer/components/CustomerInfo.tsx
@@ -3,36 +3,36 @@ import { Box, Text } from 'force-components';
 import { Customer } from '../customerSlice';
 
 interface ICustomersInfo {
-  customer?: Customer;
+  customer: Customer;
 }
 
 const CustomerInfo: FC<ICustomersInfo> = ({ customer }) => {
   return (
     <Box borderColor="colorBlue100" borderWidth="1" borderStyle="solid" p="3">
-      <Text fontWeight="600">Name: {customer?.name}</Text>
-      <Text fontSize="12px">
+      <Text fontWeight="600">Name: {customer.name}</Text>
+      <Text fontSize="12px" as="div">
         <Text as="span" fontWeight="600">
           Email:
         </Text>
-        {` ${customer?.email}`}
+        {` ${customer.email}`}
       </Text>
-      <Text fontSize="12px">
+      <Text fontSize="12px" as="div">
         <Text as="span" fontWeight="600">
           User name:
         </Text>
-        {` ${customer?.username}`}
+        {` ${customer.username}`}
       </Text>
-      <Text fontSize="12px">
+      <Text fontSize="12px" as="div">
         <Text as="span" fontWeight="600">
           Company:
         </Text>
-        {` ${customer?.company.name}`}
+        {` ${customer.company.name}`}
       </Text>
-      <Text fontSize="12px">
+      <Text fontSize="12px" as="div">
         <Text as="span" fontWeight="600">
           Phone number:
         </Text>
-        {` ${customer?.phone}`}
+        {` ${customer.phone}`}
       </Text>
     </Box>
   );
